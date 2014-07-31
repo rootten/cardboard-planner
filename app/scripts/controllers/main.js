3c36af8603178bc57dfe6b3654b69e3628d5f1ba
mmajewski <mmajewski@infusion.com>
2014-07-31 22:32:31 +0200

    fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,44 @@
 'use strict';
 
 angular.module('cardboardPlannerApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, MainService) {
+        debugger
+         MainService.getProjects().success(function(projects){
+
+             $scope.projects = projects;
+         })
+//        $scope.projects = [
+//            {name: 'sci',
+//                resources: [
+//                    {userId: 'mmajewski',
+//                        firstName: 'stanislaw',
+//                        lastName: 'palimaka'
+//                    },
+//                    {userId: 'tliptak',
+//                        firstName: 'stanislaw',
+//                        lastName: 'palimaka'
+//                    },
+//                    {userId: 'aczerski',
+//                        firstName: 'stanislaw',
+//                        lastName: 'palimaka'
+//                    }
+//                ]
+//            },
+//            {name: 'sci',
+//                resources: [
+//                    {userId: 'jlesniak',
+//                        firstName: 'stanislaw',
+//                        lastName: 'palimaka'
+//                    },
+//                    {userId: 'tliptak',
+//                        firstName: 'stanislaw',
+//                        lastName: 'palimaka'
+//                    },
+//                    {userId: 'aczerski',
+//                        firstName: 'stanislaw',
+//                        lastName: 'palimaka'
+//                    }
+//                ]
+//            }
+//        ];
+    });
