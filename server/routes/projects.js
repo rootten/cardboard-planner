1cf7c09b3dd9b62a07319df966efed4c8c2e955d
Tomasz Liptak <tliptak@infusion.com>
2014-07-31 20:49:03 +0200

    projects service

---
+++ server/routes/projects.js
@@ -0,0 +1,11 @@
+(function() {
+  module.exports = function(app) {
+
+
+ 
+    return app.get('/projects', function(req, res) {
+      return res.send('stest13afdsa');
+    });
+  };
+
+}).call(this);
