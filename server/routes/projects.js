0ea03ad4482e0eeda808f0c7250dc5e2f4e9a025
Tomasz Liptak <tliptak@infusion.com>
2014-07-31 22:13:45 +0200

    get projects service

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -5,11 +5,23 @@
 
  
     return app.get('/projects', function(req, res) {
-	  mongo.db.collection("projects").find().toArray(function(err, projects){
+	  mongo.db.collection("projects").find({}, {name: 1, resources: 1}).toArray(function(err, projects){
 		if(err){
 			throw err;
 		}
+
 		res.json(projects);
+
+		// mongo.db.collection("users").find({}, {name: 1}).toArray(function(err, users){
+		// 	if(err){
+		// 		throw err;
+		// 	}
+
+
+		// 	res.json(projects);
+		// }
+
+		//mongo.db.collection("user").
 	  })
     });
   };
